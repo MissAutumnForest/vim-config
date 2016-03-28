0d07f5a8b45748047ac22f0ec832d713127d518a
staticinteger <timothyjamespeacock@gmail.com>
2016-03-28 19:11:33 -0400

    Added a todo

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cp -R ./.vim ~/.vim
 git clone git@github.com:kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone git@github.com:scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone git@github.com:rking/ag.vim ~/.vim/bundle/ag.vim
+# TODO: write script that installs "ag" onto the system based on operating system
