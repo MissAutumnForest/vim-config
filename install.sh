29821ce9b2b5ef1f8a97df3cdc6a7143688fd063
tpeacock <tpeacock@tradeking.com>
2016-05-16 09:54:22 -0400

    Changes

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ cp -R ./.vim ~/.vim
 
 git clone git@github.com:kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 git clone git@github.com:scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+git clone git@github.com:rking/ag.vim ~/.vim/bundle/ag.vim
